c923a4ad4ebd8ffe3c792d8301dd756b47b1a5f1
Cameron Colaco <wearenotgood@gmail.com>
2021-12-15 12:01:21 -0800

    Added package.json, db, server, and csv directories via checkout from etl branch

---
+++ db/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/qanda')
+  .then(() => {
+    console.log('Mongoose connected to Q&A!');
+  })
+  .catch(error => {
+    console.log('Database Error:', error);
+  });
+
+  // SOME CHANGE HERE
